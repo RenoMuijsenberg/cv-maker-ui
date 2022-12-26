eb4e0db88a1650b9edb7647f701ea1ea83e3d998
Muijsenberg,Reno R.F <r.muijsenberg@student.fontys.nl>
2022-12-26 17:48:56 +0100

    Add animation on timeline

--- components/one/TimeLine.tsx
+++ components/one/TimeLine.tsx
@@ -1,23 +1,33 @@
 import styles from "../../styles/components/One/TimeLine.module.scss";
 import text from "../../styles/Text.module.scss";
+import {useEffect, useState} from "react";
+import classNames from "classnames";
 
 export const TimeLine = ({
 	item
 }: {
 	item: any
 }) => {
+	const [animate, setAnimate] = useState(false);
+
+	useEffect(() => {
+		setAnimate(true);
+	}, [])
+
+
+
 	return (
 		<div className={styles.timeLineContainer}>
 			<div className={styles.timeLineContainer}>
 				<div className={styles.timeLineItem}>
 					<div className={styles.leftTimeLineContainer}>
-						<div className={styles.timeLineTextPadding}>
+						<div className={classNames(styles.timeLineTextPadding, styles.animateLeft)}>
 							<p className={text.boldTextMPrimary}>{item.name}</p>
 							<p className={text.normalTextSDescription}>{item.course + " - " + item.grade}</p>
 						</div>
 					</div>
 					<div className={styles.rightTimeLineContainer}>
-						<div className={styles.timeLineTextPadding}>
+						<div className={classNames(styles.timeLineTextPadding, styles.animateRight)}>
 							<p className={text.boldTextMPrimary}>{item.time}</p>
 						</div>
 					</div>
