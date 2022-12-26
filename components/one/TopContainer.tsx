157eb16060cb3a755da873d47b0f3860521d2b05
Muijsenberg,Reno R.F <r.muijsenberg@student.fontys.nl>
2022-12-26 18:11:29 +0100

    Make top container responsive

--- components/one/TopContainer.tsx
+++ components/one/TopContainer.tsx
@@ -1,14 +1,13 @@
 import styles from "../../styles/components/One/TopContainer.module.scss";
 import text from "../../styles/Text.module.scss";
-import {Socials} from "./Socials";
 import {DiagonalLine} from "./DiagonalLine";
-import {Divide, Divide as Hamburger} from 'hamburger-react'
+import {Divide as Hamburger} from 'hamburger-react'
 
 export const TopContainer = ({image, name, profession}: {image: string, name: string, profession: string}) => {
     return (
         <div className={styles.topContainer}>
             <div className={styles.hamburger}>
-                <Hamburger/>
+                <Hamburger size={28}/>
             </div>
             <img className={styles.userImage} src={image} alt={`Image of ${name}`}/>
             <div className={styles.introText}>
